85017701683799514c2abf3caefc9bb83a090c23
Andrew Robinson <robinson.andrewjames@gmail.com>
2025-07-17 12:47:57 +0100

    updated logo bar styles.

--- src/components/top-bar/top-bar-feature.tsx
+++ src/components/top-bar/top-bar-feature.tsx
@@ -1,9 +1,24 @@
 import { View, Text } from "react-native";
 import { TopBarWalletMenu } from "./top-bar-ui";
+import theme from '../../theme';
 
 export function TopBar() {
   return (
-    <View className="bg-transparent h-20 flex-row px-4 items-center justify-between">
+    <View style={{
+      backgroundColor: theme.colors.surfaceContainerHigh,
+      height: 80,
+      flexDirection: 'row',
+      paddingHorizontal: 16,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+      shadowColor: '#000',
+      shadowOpacity: 0.12,
+      shadowRadius: 0,
+      shadowOffset: { width: 0, height: 0 },
+      elevation: 8,
+    }}>
       <Text className="text-white text-[24px] font-better-extra-bold">BetrWeather</Text>
       <TopBarWalletMenu />
     </View>
