3f9e5f49db2e7cbae5d80d46e4f5ec384091913f
Andrew Robinson <robinson.andrewjames@gmail.com>
2025-10-14 02:28:46 +0100

    fix worklets error for build.

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   assets: ['./assets/fonts/'],
+  // Disable autolinking for transitive worklets package which is incompatible with RN 0.76
+  dependencies: {
+    'react-native-worklets': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
 };
